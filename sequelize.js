024a3a94ec728150884246cc22292da812f30d51
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-06 13:46:45 -0500

    Added Models

---
+++ sequelize.js
@@ -0,0 +1,29 @@
+import { Sequelize, DataTypes } from "sequelize";
+import dotenv from "dotenv";
+import { UserModel } from "./models/UserModel.js";
+
+dotenv.config();
+
+export const sequelize = new Sequelize( 
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {   
+        host: process.env.DB_HOST,     
+        dialect: process.env.DB_DIALECT,
+    }
+);
+
+export const User = UserModel(sequelize, DataTypes);
+
+export const syncdb = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Connection established');
+        await sequelize.sync({alter: true});
+        console.log('Database synchronized');
+    }
+    catch(err){
+        console.log('Error: ', err);
+    }
+}
