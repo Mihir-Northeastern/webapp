456ad1859180a7ef7a65bdd2776751176f234a1b
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-07 00:21:19 -0500

    Api get, update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,28 @@
 import * as userService from '../services/userService.js';
 
+export const getAllUser = async (req, res) => {
+    try {
+        const users = await userService.getAllUser();
+        res.status(200).send(users);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send();
+    }
+}
 
+export const getUserById = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        if (user) {
+            res.status(200).send(user);
+        } else {
+            res.status(404).send();
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send();
+    }
+}
 
 export const createUser = async (req, res) => {
     try {
@@ -11,3 +33,17 @@ export const createUser = async (req, res) => {
         res.status(500).send();
     }
 };
+
+export const updateUser = async (req, res) => {
+    if (Object.keys(req.body).length === 0) {
+        res.status(204).send();
+        return;
+    }
+    try {
+        const updatedUser = await userService.updateUser(req.params.id, req.body);
+        res.status(200).send(updatedUser);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send();
+    }
+}
