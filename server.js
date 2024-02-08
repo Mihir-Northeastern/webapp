cf3282ca080d2f6289c8a2b5f91590d8df339664
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-07 21:31:30 -0500

    fixed jest error

--- server.js
+++ server.js
@@ -1,42 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import initializeRoutes from './routes/index.js';
 import { syncdb } from './sequelize.js';
+import app from './app.js';
+import dotenv from 'dotenv';
 
 // Load the environment variables from .env file
 dotenv.config();
-
-const app = express();
-
-//Adding No cache for the required headers
-app.use((req, res, next) => {
-    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.set('Pragma', 'no-cache');
-    res.set('X-Content-Type-Options', 'nosniff');
-    next();
-})
-
-//Middleware to parse the request body
-app.use((express.json()));
-
-
-app.use((error, req, res, next) => {
-    if (error instanceof SyntaxError) {
-        res.status(400).send();
-    } else {
-        next();
-    }
-});
-
-// Middleware to parse the URL encoded data
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to enable CORS
-app.use(cors());
-
-initializeRoutes(app);
-
 
 
 app.listen(process.env.PORT, async() => {    
