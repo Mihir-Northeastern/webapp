b05fb5f772fa675729d5f3dfe00b3ce46026b180
Mihir2208 <118858022+Mihir2208@users.noreply.github.com>
2024-02-10 20:02:25 -0500

    Update healthz.test.js

--- test/healthz.test.js
+++ test/healthz.test.js
@@ -4,6 +4,6 @@ import app from '../app.js';
 describe('GET /healthz', () => {
     it('should return 200 OK', async () => {
         const response = await request(app).get('/healthz');
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(500);
     });
 });
