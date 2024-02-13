e595177ae8aef63b1adfb073d57034e62b5c7052
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-12 20:51:38 -0500

    testcase added

--- test/healthz.test.js
+++ test/healthz.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from '../app.js';
+import btoa from 'btoa';
 
 describe('GET /healthz', () => {
     it('should return 200 OK', async () => {
@@ -7,3 +8,46 @@ describe('GET /healthz', () => {
         expect(response.statusCode).toBe(200);
     });
 });
+
+
+const userData = {
+    first_name: "Narendra",
+    last_name: "Makwana",
+    username: "n.m@gmail.om",
+    password: "1234" 
+};
+
+const base64Credentials = btoa(`${userData.username}:${userData.password}`);
+
+describe('POST /v1/user/', () => {
+    it('should create an user', async () => {
+      
+        const response = await request(app)
+            .post('/v1/user/')
+            .send(userData);
+        expect(response.statusCode).toBe(201);
+
+        const userFromApi = await request(app)
+        .get('/v1/user/self')
+        .set('Authorization', `Basic ${base64Credentials}`);
+        expect(userFromApi.statusCode).toBe(200);
+    });
+});
+
+describe('PUT /v1/user/self', () => {
+    it('should update an user', async () => {
+        const updateUser = {
+                first_name: "Seema",
+        };
+        const response = await request(app)
+            .put('/v1/user/self')
+            .set('Authorization', `Basic ${base64Credentials}`)
+            .send(updateUser);
+        expect(response.statusCode).toBe(204);
+   
+        const userFromApi = await request(app)
+        .get('/v1/user/self')
+        .set('Authorization', `Basic ${base64Credentials}`);
+        expect(userFromApi.body.first_name).toBe(updateUser.first_name);
+    });
+});    
