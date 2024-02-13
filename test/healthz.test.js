be560124d20de30f78aef16a6ce2a815f66cbcde
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-12 20:59:57 -0500

    bug fix

--- test/healthz.test.js
+++ test/healthz.test.js
@@ -1,6 +1,11 @@
 import request from 'supertest';
 import app from '../app.js';
 import btoa from 'btoa';
+import { syncdb } from '../sequelize.js';
+
+beforeAll(async () => {
+    await syncdb();
+});
 
 describe('GET /healthz', () => {
     it('should return 200 OK', async () => {
@@ -13,7 +18,7 @@ describe('GET /healthz', () => {
 const userData = {
     first_name: "Narendra",
     last_name: "Makwana",
-    username: "n.m@gmail.om",
+    username: "n.m@gmil.om",
     password: "1234" 
 };
 
